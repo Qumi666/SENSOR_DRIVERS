92dc25ce1762dacfc6d2431b659d4749be84de43
ChenQiming <102596959+Qumi666@users.noreply.github.com>
2025-05-18 01:26:23 +0800

    Add files via upload

--- MPU6050/Inc/mpu6050.h
+++ MPU6050/Inc/mpu6050.h
@@ -1,18 +1,24 @@
-#ifndef __MPU6050_H__
-#define __MPU6050_H__
+/*
+ * mpu6050.h
+ *
+ *  Created on: May 5, 2025
+ *      Author: 59788
+ */
+
+#ifndef INC_MPU6050_H_
+#define INC_MPU6050_H_
 
 //header included
 #include "stm32f1xx_hal.h"
 #include "main.h"
 #include "i2c.h"
 #include"math.h"
-
-//register address refer to datasheet
-#define MPU6050_ADDR 0xD0//slave address
-#define SMPLRT_DIV_REG 0x19
+//register define
+#define MPU6050_ADDR 0xD0//从机地址
+#define SMPLRT_DIV_REG 0x19//采样率
 #define CONFIG 0x1A
-#define GYRO_CONFIG_REG 0x1B
-#define ACCEL_CONFIG_REG 0x1C
+#define GYRO_CONFIG_REG 0x1B//陀螺仪
+#define ACCEL_CONFIG_REG 0x1C//加速度
 #define ACCEL_XOUT_H_REG 0x3B
 #define TEMP_OUT_H_REG 0x41
 #define GYRO_XOUT_H_REG 0x43
@@ -20,13 +26,14 @@
 #define PWR_MGMT_2_REG 0x6C
 #define WHO_AM_I_REG 0x75
 
-//function
+//这是咱们的函数，初始化，读A，读G
 void MPU6050_Init(void);
 void MPU6050_Read_Accel(void);
 void MPU6050_Read_Gyro(void);
-void MPU6050_Read_Result(void);//Euler Angles roll, pitch, yaw
-
-extern float Ax, Ay, Az, Gx, Gy, Gz,roll, pitch, yaw;
+void MPU6050_Read_Result(void);//读roll, pitch, yaw
 
 
-#endif
+//导出，导出后数据才能被其他程序应用
+extern float Ax, Ay, Az, Gx, Gy, Gz,roll, pitch, yaw;//A代表加速度，G代表角速度
+
+#endif /* INC_MPU6050_H_ */
