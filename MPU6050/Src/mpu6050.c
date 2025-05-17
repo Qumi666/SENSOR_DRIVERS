28ec70f28f97942fc87a32a162e4ba14649bef52
ChenQiming <102596959+Qumi666@users.noreply.github.com>
2025-05-18 01:22:38 +0800

    Add files via upload

--- MPU6050/Src/mpu6050.c
+++ MPU6050/Src/mpu6050.c
@@ -1,10 +1,18 @@
+/*
+ * mpu6050.c
+ *
+ *  Created on: May 5, 2025
+ *      Author: 59788
+ */
+#include "i2c.h"
 #include "mpu6050.h"
-#include "math.h"
 
-//raw data
-int16_t Accel_X_RAW = 0;
-int16_t Accel_Y_RAW = 0;
-int16_t Accel_Z_RAW = 0;
+#define MPU6050_ADDRESS 0xD0
+
+int16_t Acc_X_RAW = 0;
+int16_t Acc_Y_RAW = 0;
+int16_t Acc_Z_RAW = 0;
+
 int16_t Gyro_X_RAW = 0;
 int16_t Gyro_Y_RAW = 0;
 int16_t Gyro_Z_RAW = 0;
@@ -18,7 +26,8 @@ float roll_a, pitch_a;
 //使用角速度计算《角速度欧拉角》，先定义几个变量。_g的意思就是用的角速度
 float roll_g, pitch_g, yaw_g;
 
-//Euler Angles
+
+//欧拉角
 float roll, pitch, yaw;
 
 
@@ -28,54 +37,55 @@ void MPU6050_Init(void)//初始化
 	uint8_t check;
 	uint8_t Data;
 
-	HAL_I2C_Mem_Read(&hi2c2, MPU6050_ADDR, WHO_AM_I_REG, 1, &check, 1, 1000);
+	HAL_I2C_Mem_Read(&hi2c1, MPU6050_ADDR, WHO_AM_I_REG, 1, &check, 1, 1000);
 	//注意。我的板子I2C1给显示屏了，所以给陀螺仪的是I2C2，故用&hi2c2
 
 	if (check == 0x68)//寄存器文档最后一页写了WHO_AM_I_REG就是0x68
 	{
 		//电源管理1
 		Data = 0x01;
-		HAL_I2C_Mem_Write(&hi2c2, MPU6050_ADDR, PWR_MGMT_1_REG, 1, &Data, 1, 1000);
+		HAL_I2C_Mem_Write(&hi2c1, MPU6050_ADDR, PWR_MGMT_1_REG, 1, &Data, 1, 1000);
 
 		//电源管理2
 		Data = 0x00;
-		HAL_I2C_Mem_Write(&hi2c2, MPU6050_ADDR, PWR_MGMT_2_REG, 1, &Data, 1, 1000);
+		HAL_I2C_Mem_Write(&hi2c1, MPU6050_ADDR, PWR_MGMT_2_REG, 1, &Data, 1, 1000);
 
 		//滤波
 		Data = 0x06;
-		HAL_I2C_Mem_Write(&hi2c2, MPU6050_ADDR, CONFIG, 1, &Data, 1, 1000);
+		HAL_I2C_Mem_Write(&hi2c1, MPU6050_ADDR, CONFIG, 1, &Data, 1, 1000);
 
 
 		//采样频率分频器寄存器
 		Data = 0x09;
-		HAL_I2C_Mem_Write(&hi2c2, MPU6050_ADDR, SMPLRT_DIV_REG, 1, &Data, 1, 1000);
+		HAL_I2C_Mem_Write(&hi2c1, MPU6050_ADDR, SMPLRT_DIV_REG, 1, &Data, 1, 1000);
 
 		//加速度计配置
 		Data = 0x18;
-		HAL_I2C_Mem_Write(&hi2c2, MPU6050_ADDR, ACCEL_CONFIG_REG, 1, &Data, 1, 1000);
+		HAL_I2C_Mem_Write(&hi2c1, MPU6050_ADDR, ACCEL_CONFIG_REG, 1, &Data, 1, 1000);
 
 		//陀螺仪配置
 		Data = 0x18;
-		HAL_I2C_Mem_Write(&hi2c2, MPU6050_ADDR, GYRO_CONFIG_REG, 1, &Data, 1, 1000);
+		HAL_I2C_Mem_Write(&hi2c1, MPU6050_ADDR, GYRO_CONFIG_REG, 1, &Data, 1, 1000);
 	}
 }
 
-void MPU6050_Read_Accel(void)
+void MPU6050_Read_Accel(void)//读取加速度
 {
 	uint8_t Rec_Data[6];
-	HAL_I2C_Mem_Read(&hi2c2, MPU6050_ADDR, ACCEL_XOUT_H_REG, 1, Rec_Data, 6, 1000);
-	Accel_X_RAW = (int16_t)(Rec_Data[0] << 8 | Rec_Data[1]);
-	Accel_Y_RAW = (int16_t)(Rec_Data[2] << 8 | Rec_Data[3]);
-	Accel_Z_RAW = (int16_t)(Rec_Data[4] << 8 | Rec_Data[5]);
-	Ax = Accel_X_RAW / 2048.0;
-	Ay = Accel_Y_RAW / 2048.0;
-	Az = Accel_Z_RAW / 2048.0;
+	HAL_I2C_Mem_Read(&hi2c1, MPU6050_ADDR, ACCEL_XOUT_H_REG, 1, Rec_Data, 6, 1000);
+	Acc_X_RAW = (int16_t)(Rec_Data[0] << 8 | Rec_Data[1]);
+	Acc_Y_RAW = (int16_t)(Rec_Data[2] << 8 | Rec_Data[3]);
+	Acc_Z_RAW = (int16_t)(Rec_Data[4] << 8 | Rec_Data[5]);
+	Ax = Acc_X_RAW / 2048.0;
+	Ay = Acc_Y_RAW / 2048.0;
+	Az = Acc_Z_RAW / 2048.0;//此处多减0.5是我的硬件问题，如果你硬件是好的，就不用减
 }
+//此处请特别注意！！！！！！我硬件有问题减了0.5！！！！你们用的时候可以先不减
 
 void MPU6050_Read_Gyro(void)//读取角速度
 {
 	uint8_t Rec_Data[6];
-	HAL_I2C_Mem_Read(&hi2c2, MPU6050_ADDR, GYRO_XOUT_H_REG, 1, Rec_Data, 6, 1000);
+	HAL_I2C_Mem_Read(&hi2c1, MPU6050_ADDR, GYRO_XOUT_H_REG, 1, Rec_Data, 6, 1000);
 	Gyro_X_RAW = (int16_t)(Rec_Data[0] << 8 | Rec_Data[1]);
 	Gyro_Y_RAW = (int16_t)(Rec_Data[2] << 8 | Rec_Data[3]);
 	Gyro_Z_RAW = (int16_t)(Rec_Data[4] << 8 | Rec_Data[5]);
@@ -84,20 +94,24 @@ void MPU6050_Read_Gyro(void)//读取角速度
 	Gz = Gyro_Z_RAW / 16.4;
 }
 
-//Complementary Filtering for Eular Angle
-void MPU6050_Read_Result(void)
-{
-	uint8_t Rec_Data_A[6];
-	HAL_I2C_Mem_Read(&hi2c2, MPU6050_ADDR, ACCEL_XOUT_H_REG, 1, Rec_Data_A, 6, 1000);
-	Accel_X_RAW = (int16_t)(Rec_Data_A[0] << 8 | Rec_Data_A[1]);
-	Accel_Y_RAW = (int16_t)(Rec_Data_A[2] << 8 | Rec_Data_A[3]);
-	Accel_Z_RAW = (int16_t)(Rec_Data_A[4] << 8 | Rec_Data_A[5]);
-	Ax = Accel_X_RAW / 2048.0;
-	Ay = Accel_Y_RAW / 2048.0;
-	Az = Accel_Z_RAW / 2048.0;
 
+//此函数是读取结果，当然这个结果是互补滤波法计算的
+//如果你想读取yaw roll pitch 仅需调用此函数
+void MPU6050_Read_Result(void){
+
+	//上面函数MPU6050_Read_Accel()中的，改了下变量名，让大家更易懂，你也可以直接调用函数
+	uint8_t Rec_Data_A[6];
+	HAL_I2C_Mem_Read(&hi2c1, MPU6050_ADDR, ACCEL_XOUT_H_REG, 1, Rec_Data_A, 6, 1000);
+	Acc_X_RAW = (int16_t)(Rec_Data_A[0] << 8 | Rec_Data_A[1]);
+	Acc_Y_RAW = (int16_t)(Rec_Data_A[2] << 8 | Rec_Data_A[3]);
+	Acc_Z_RAW = (int16_t)(Rec_Data_A[4] << 8 | Rec_Data_A[5]);
+	Ax = Acc_X_RAW / 2048.0;
+	Ay = Acc_Y_RAW / 2048.0;
+	Az = Acc_Z_RAW / 2048.0;//此处多减0.5是我的硬件问题，如果你硬件是好的，就不用减
+
+	//上面函数MPU6050_Read_Gyro()中的，改了下变量名，让大家更易懂，你也可以直接调用函数
 	uint8_t Rec_Data_G[6];
-	HAL_I2C_Mem_Read(&hi2c2, MPU6050_ADDR, GYRO_XOUT_H_REG, 1, Rec_Data_G, 6, 1000);
+	HAL_I2C_Mem_Read(&hi2c1, MPU6050_ADDR, GYRO_XOUT_H_REG, 1, Rec_Data_G, 6, 1000);
 	Gyro_X_RAW = (int16_t)(Rec_Data_G[0] << 8 | Rec_Data_G[1]);
 	Gyro_Y_RAW = (int16_t)(Rec_Data_G[2] << 8 | Rec_Data_G[3]);
 	Gyro_Z_RAW = (int16_t)(Rec_Data_G[4] << 8 | Rec_Data_G[5]);
@@ -120,13 +134,6 @@ void MPU6050_Read_Result(void)
 	roll = roll + (roll_a - roll_g ) *alpha;
 	pitch = pitch + (pitch_a - pitch_g) *alpha;
 	yaw = yaw_g;
-	
+
 
 }
-
-
-
-
-
-
-
